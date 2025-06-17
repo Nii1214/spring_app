71aaf7f455ac839824447c13f53d249379a348af
Nii1214 <nii.tomoyuki1214@gmail.com>
2025-06-17 18:45:29 +0900

    feat:ユーザー関連の制御ファイルを作成

--- src/main/java/com/example/spring_app/service/CustomUserDetailsService.java
+++ src/main/java/com/example/spring_app/service/CustomUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.spring_app.entity.User;
 import com.example.spring_app.repository.UserRepository;
+import com.example.spring_app.security.CustomUserDetails;
 
 import java.util.Collections;
 
@@ -29,7 +30,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         String role = user.getRoleId() == 2 ? "ROLE_ADMIN" : "ROLE_USER";
 
-        return new org.springframework.security.core.userdetails.User(
+        return new CustomUserDetails(
+            user.getUserId().longValue(),
             user.getUserName(),
             user.getPassword(),
             Collections.singletonList(new SimpleGrantedAuthority(role))
